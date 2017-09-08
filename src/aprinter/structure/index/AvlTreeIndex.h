e39aaa79e49e35fc8c52eebc80f43f450f5370d6
Ambroz Bizjak <ambrop7@gmail.com>
2017-09-09 00:45:24 +0200

    Add index data structures.

---
+++ src/aprinter/structure/index/AvlTreeIndex.h
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2017 Ambroz Bizjak
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef APRINTER_AVL_TREE_INDEX_H
+#define APRINTER_AVL_TREE_INDEX_H
+
+#include <aprinter/meta/ServiceUtils.h>
+#include <aprinter/base/Preprocessor.h>
+#include <aprinter/base/Accessor.h>
+#include <aprinter/base/Assert.h>
+#include <aprinter/structure/LinkModel.h>
+#include <aprinter/structure/AvlTree.h>
+#include <aprinter/structure/TreeCompare.h>
+
+namespace APrinter {
+
+template <typename Arg>
+class AvlTreeIndex {
+    APRINTER_USE_TYPES1(Arg, (HookAccessor, LookupKeyArg, KeyFuncs, LinkModel))
+    
+    APRINTER_USE_TYPES1(LinkModel, (State, Ref))
+    
+    using TreeNode = AvlTreeNode<LinkModel>;
+    
+public:
+    class Node {
+        friend AvlTreeIndex;
+        
+        TreeNode tree_node;
+    };
+    
+    class Index {
+        using TreeNodeAccessor = ComposedAccessor<
+            HookAccessor,
+            APRINTER_MEMBER_ACCESSOR_TN(&Node::tree_node)
+        >;
+        
+        struct TheTreeCompare : public TreeCompare<LinkModel, KeyFuncs> {};
+        
+        using EntryTree = AvlTree<TreeNodeAccessor, TheTreeCompare, LinkModel>;
+        
+    public:
+        inline void init ()
+        {
+            m_tree.init();
+        }
+        
+        inline void addEntry (Ref e, State st = State())
+        {
+            bool inserted = m_tree.insert(e, nullptr, st);
+            AMBRO_ASSERT(inserted)
+        }
+        
+        inline void removeEntry (Ref e, State st = State())
+        {
+            m_tree.remove(e, st);
+        }
+        
+        inline Ref findEntry (LookupKeyArg key, State st = State())
+        {
+            Ref entry = m_tree.template lookup<LookupKeyArg>(key, st);
+            AMBRO_ASSERT(entry.isNull() ||
+                         KeyFuncs::KeysAreEqual(KeyFuncs::GetKeyOfEntry(*entry), key))
+            return entry;
+        }
+        
+        inline Ref first (State st = State())
+        {
+            return m_tree.first(st);
+        }
+        
+        inline Ref next (Ref node, State st = State())
+        {
+            return m_tree.next(node, st);
+        }
+        
+    private:
+        EntryTree m_tree;
+    };
+};
+
+struct AvlTreeIndexService {
+    APRINTER_ALIAS_STRUCT_EXT(Index, (
+        APRINTER_AS_TYPE(HookAccessor),
+        APRINTER_AS_TYPE(LookupKeyArg),
+        APRINTER_AS_TYPE(KeyFuncs),
+        APRINTER_AS_TYPE(LinkModel)
+    ), (
+        APRINTER_DEF_INSTANCE(Index, AvlTreeIndex)
+    ))
+};
+
+}
+
+#endif
