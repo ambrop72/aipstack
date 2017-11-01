691e89f8a04c9ec7aab96e8306f9b19721268e98
Ambroz Bizjak <ambrop7@gmail.com>
2017-11-01 13:01:49 +0100

    Document Hints.h

--- src/aipstack/misc/Hints.h
+++ src/aipstack/misc/Hints.h
@@ -25,10 +25,107 @@
 #ifndef AIPSTACK_HINTS_H
 #define AIPSTACK_HINTS_H
 
-#ifdef __GNUC__
+/**
+ * @ingroup misc
+ * @defgroup hints Optimization Hints
+ * @brief Optimization hints for compilers.
+ * 
+ * The macros in this module may involve compiler-specific optimization hints, but no effect
+ * is guaranteed. The definitions given in this documentation are what these macros are
+ * functionally equivalent to.
+ * 
+ * The macros @ref AIPSTACK_LIKELY and @ref AIPSTACK_UNLIKELY provide hints to the compiler
+ * to optimize for the case where the expression evaluates to true or false respectively.
+ * They are most commonly used within `if` clauses, for example:
+ * 
+ * ```
+ * if (AIPSTACK_LIKELY(x == 5)) {
+ *     // ...
+ * }
+ * if (AIPSTACK_UNLIKELY(ptr == nullptr) {
+ *     // ...
+ * }
+ * ```
+ * 
+ * The macros @ref AIPSTACK_ALWAYS_INLINE, @ref AIPSTACK_NO_INLINE, @ref AIPSTACK_NO_RETURN,
+ * @ref AIPSTACK_UNROLL_LOOPS and @ref AIPSTACK_OPTIMIZE_SIZE can be understood as function
+ * attributes and are used in the list of qualifiers before the return type. For a template
+ * function, that would be after the template parameters. See the examples below.
+ * 
+ * ```
+ * AIPSTACK_ALWAYS_INLINE
+ * int function1 (int arg) {
+ *     return arg + 1;
+ * }
+ * 
+ * template <typename T>
+ * AIPSTACK_NO_INLINE
+ * void function2 (T arg) {
+ *     arg.foo();
+ * }
+ * ```
+ * @{
+ */
 
-#define AIPSTACK_LIKELY(x) __builtin_expect((x), 1)
-#define AIPSTACK_UNLIKELY(x) __builtin_expect((x), 0)
+#if defined(IN_DOXYGEN) || !defined(__GNUC__)
+
+/**
+ * Convert to bool and optimize for the true case.
+ * 
+ * @param x Input value.
+ * @return The value converted to boolean.
+ */
+#define AIPSTACK_LIKELY(x) (!!(x))
+
+/**
+ * Convert to bool and optimize for the false case.
+ * 
+ * @param x Input value.
+ * @return The value converted to boolean.
+ */
+#define AIPSTACK_UNLIKELY(x) (!!(x))
+
+/**
+ * Make a function inline and prefer to actually inline it.
+ * 
+ * This is used in the list of qualifiers before the return type. The `inline` qualifier is
+ * included and should not be specified directly when using this macro.
+ */
+#define AIPSTACK_ALWAYS_INLINE inline
+
+/**
+ * Prefer not to inline a function.
+ * 
+ * This is used in the list of qualifiers before the return type.
+ */
+#define AIPSTACK_NO_INLINE
+
+/**
+ * Indicate that the function never returns.
+ * 
+ * This is used in the list of qualifiers before the return type. This may only be used when
+ * the function does in fact never return.
+ */
+#define AIPSTACK_NO_RETURN
+
+/**
+ * Prefer to unroll loops in a function.
+ * 
+ * This is used in the list of qualifiers before the return type.
+ */
+#define AIPSTACK_UNROLL_LOOPS
+
+/**
+ * Prefer to optimize the function for code size.
+ * 
+ * This is used in the list of qualifiers before the return type.
+ */
+#define AIPSTACK_OPTIMIZE_SIZE
+
+#else
+
+#define AIPSTACK_LIKELY(x) __builtin_expect(!!(x), 1)
+#define AIPSTACK_UNLIKELY(x) __builtin_expect(!!(x), 0)
 #define AIPSTACK_ALWAYS_INLINE __attribute__((always_inline)) inline
 #define AIPSTACK_NO_INLINE __attribute__((noinline))
 #define AIPSTACK_NO_RETURN __attribute__((noreturn))
@@ -41,16 +138,8 @@
 #define AIPSTACK_OPTIMIZE_SIZE
 #endif
 
-#else
-
-#define AIPSTACK_LIKELY(x) (x)
-#define AIPSTACK_UNLIKELY(x) (x)
-#define AIPSTACK_ALWAYS_INLINE
-#define AIPSTACK_NO_INLINE
-#define AIPSTACK_NO_RETURN
-#define AIPSTACK_UNROLL_LOOPS
-#define AIPSTACK_OPTIMIZE_SIZE
-
 #endif
+
+/** @} */
 
 #endif
