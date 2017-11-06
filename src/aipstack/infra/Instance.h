d71c07efa76d6abc993d9efac5d90793b53945d2
Ambroz Bizjak <ambrop7@gmail.com>
2017-11-06 19:45:38 +0100

    Document Instance.h

--- src/aipstack/infra/Instance.h
+++ src/aipstack/infra/Instance.h
@@ -28,20 +28,101 @@
 #include <aipstack/misc/Preprocessor.h>
 
 /**
- * @addtogroup infra
+ * @ingroup infra
+ * @defgroup instance Template Class Instantiation
+ * @brief Mechanism to instantiate class templates with an identity.
+ * 
+ * The purpose of this mechanism is to prevent the explosion of template type expressions in
+ * symbol names, especially within debug symbols. For example, consider a class which is
+ * configured using the @ref configuration module:
+ * 
+ * ```
+ * template <typename... Options>
+ * class MyClass {
+ *     AIPSTACK_OPTION_CONFIG_VALUE(MyClassOptions, Option1)
+ * public:
+ *     class NestedClass {};
+ *     void function(NestedClass n);
+ * };
+ * ```
+ * 
+ * If the class template is instantiated as `MyClass<MyClassOptions::Option1::Is<5>>`, then
+ * the full signature of `function` is:
+ * 
+ * ```
+ * void MyClass<MyClassOptions::Option1::Is<5>>::function(MyClass<MyClassOptions::Option1::Is<5>>::NestedClass)
+ * ```
+ * 
+ * As can be seen, the entire configuration of the class is repeated twice in the signature
+ * of `function`. In a real application where modules have many options and are instantiated
+ * hierarchially, such native use of the configuration system can easily result in an
+ * explosion of signature lengths, causing extreme memory usage during compilation/linking
+ * and extreme binary size.
+ * 
+ * This mechanism provides macros which solve this problem by effectively giving each
+ * instatiation an identity, represented by a class whose signature does not include the
+ * configuration options. In plain C++ code, the solution is as follows:
+ * 
+ * ```
+ * template <typename Arg>
+ * class MyClass {
+ *     // Arg is a type derived from MyClassService, so option can be accessed like this:
+ *     // Arg::Option1
+ * public:
+ *     class NestedClass {};
+ *     void function(NestedClass n);
+ * };
+ * 
+ * template <typename... Options>
+ * class MyClassService {
+ *     AIPSTACK_OPTION_CONFIG_VALUE(MyClassOptions, Option1)
+ *     
+ *     template <typename Instance_self=MyClassService>
+ *     using Instance = MyClass<Instance_self>;
+ *     // The type alias above should instead be defined with a macro like this:
+ *     //AIPSTACK_DEF_INSTANCE(MyClassService, MyClass)
+ * };
+ * 
+ * // At the site of instantiation:
+ * struct MyClassInstance_arg : public MyClassService<MyClassOptions::Option1::Is<5>> {};
+ * using MyClassInstance = typename MyClassInstance_arg::template Instance<MyClassInstance_arg>;
+ * // The struct and type alias above should instead be defined with a macro like this:
+ * //AIPSTACK_MAKE_INSTANCE(MyClassInstance, (MyClassService<MyClassOptions::Option1::Is<5>>))
+ * ```
+ * 
+ * The resulting `MyClassInstance` is a type alias for `MyClass<MyClassInstance_arg>`.
+ * The expression of this type does not contain the configuration options, which are hidden
+ * as the base class type of `MyClassInstance_arg`. When this mechanism is used consistently,
+ * the explosion of symbol lengths is prevented.
+ * 
  * @{
  */
 
 /**
- * Convenience macro for declaring the Instance template-alias
- * in a service definition.
+ * Define a template alias allowing definition of named instances using
+ * @ref AIPSTACK_MAKE_INSTANCE.
+ * 
+ * See the @ref instance module description.
+ * 
+ * @param self Type which will be the base class of types passed to the `class` class
+ *        template. This should be the name of the class where this macro is used.
+ * @param class Class template which accepts a single type parameter, which will be a
+ *        different trivial struct type for each instance, derived from `self`.
  */
 #define AIPSTACK_DEF_INSTANCE(self, class) \
 template <typename Instance_self=self> \
 using Instance = class<Instance_self>;
 
 /**
- * Convenience macro for instantiating a service instance.
+ * Define a named instance based on a previous @ref AIPSTACK_DEF_INSTANCE definition.
+ * 
+ * See the @ref instance module description.
+ * 
+ * @param service_name Name of the type alias which will represent the instantiated type.
+ * @param arg_expr_parens Class type which contains the @ref AIPSTACK_DEF_INSTANCE
+ *        definition. It must be specified within parentheses; this allows using commas
+ *        between template parameters, which would otherwise be treated as delimiting
+ *        macro arguments.
  */
 #define AIPSTACK_MAKE_INSTANCE(service_name, arg_expr_parens) \
 struct service_name##_arg : public AIPSTACK_REMOVE_PARENS arg_expr_parens {}; \
