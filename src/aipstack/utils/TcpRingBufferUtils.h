afd17ad3811ad23c32a94733290db4310e80a637
Ambroz Bizjak <ambrop7@gmail.com>
2017-11-24 19:01:29 +0100

    TcpRingBufferUtils: Expose IpBufRef instead of WrapBuffer
    And delete WrapBuffer for good.

--- src/aipstack/utils/TcpRingBufferUtils.h
+++ src/aipstack/utils/TcpRingBufferUtils.h
@@ -28,184 +28,218 @@
 #include <stddef.h>
 #include <string.h>
 
-#include <aipstack/misc/Use.h>
 #include <aipstack/misc/Assert.h>
 #include <aipstack/misc/MinMax.h>
+#include <aipstack/misc/Modulo.h>
+#include <aipstack/misc/Hints.h>
 #include <aipstack/infra/Buf.h>
 #include <aipstack/infra/MemRef.h>
-#include <aipstack/utils/WrapBuffer.h>
 
 namespace AIpStack {
 
 template <typename TcpProto>
-class TcpRingBufferUtils {
-    AIPSTACK_USE_TYPES(TcpProto, (SeqType, Connection))
-    
+class SendRingBuffer {
 public:
-    class SendRingBuffer {
-    public:
-        void setup (Connection &con, char *buf, size_t buf_size)
-        {
-            AIPSTACK_ASSERT(buf != nullptr)
-            AIPSTACK_ASSERT(buf_size > 0)
-            
-            m_buf_node = IpBufNode{buf, buf_size, &m_buf_node};
-            
-            con.setSendBuf(IpBufRef{&m_buf_node, (size_t)0, (size_t)0});
-        }
+    using Connection = typename TcpProto::Connection;
+
+    void setup (Connection &con, char *buf, size_t buf_size)
+    {
+        AIPSTACK_ASSERT(buf != nullptr)
+        AIPSTACK_ASSERT(buf_size > 0)
+        AIPSTACK_ASSERT(buf_size >= con.getSendBuf().tot_len)
         
-        size_t getFreeLen (Connection &con)
-        {
-            return m_buf_node.len - get_send_buf(con).tot_len;
-        }
+        m_buf_node = IpBufNode{buf, buf_size, &m_buf_node};
         
-        WrapBuffer getWritePtr (Connection &con)
-        {
-            IpBufRef snd_buf = get_send_buf(con);
-            size_t write_offset = add_modulo(snd_buf.offset, snd_buf.tot_len, m_buf_node.len);
-            return WrapBuffer(m_buf_node.len - write_offset, m_buf_node.ptr + write_offset, m_buf_node.ptr);
+        IpBufRef old_send_buf = con.getSendBuf();
+
+        IpBufRef send_buf = IpBufRef{&m_buf_node, (size_t)0, old_send_buf.tot_len};
+
+        if (old_send_buf.tot_len > 0) {
+            IpBufRef tmp_buf = send_buf;
+            tmp_buf.giveBuf(old_send_buf);
         }
-        
-        void provideData (Connection &con, size_t amount)
-        {
-            AIPSTACK_ASSERT(amount <= getFreeLen(con))
-            
-            con.extendSendBuf(amount);
-        }
-        
-        void writeData (Connection &con, MemRef data)
-        {
-            AIPSTACK_ASSERT(data.len <= getFreeLen(con))
-            
-            getWritePtr(con).copyIn(data);
-            con.extendSendBuf(data.len);
-        }
-        
-    private:
-        inline IpBufRef get_send_buf (Connection &con)
-        {
-            IpBufRef snd_buf = con.getSendBuf();
-            AIPSTACK_ASSERT(snd_buf.tot_len <= m_buf_node.len)
-            AIPSTACK_ASSERT(snd_buf.offset < m_buf_node.len) // < due to eager buffer consumption
-            return snd_buf;
-        }
-        
-    private:
-        IpBufNode m_buf_node;
-    };
+
+        con.setSendBuf(send_buf);
+    }
     
-    class RecvRingBuffer {
-    public:
-        // NOTE: If using mirror region and initial_rx_data is not empty, it is
-        // you may need to call updateMirrorAfterDataReceived to make sure initial
-        // data is mirrored as applicable.
-        void setup (Connection &con, char *buf, size_t buf_size, int wnd_upd_div,
-                    IpBufRef initial_rx_data)
-        {
-            AIPSTACK_ASSERT(buf != nullptr)
-            AIPSTACK_ASSERT(buf_size > 0)
-            AIPSTACK_ASSERT(wnd_upd_div >= 2)
-            AIPSTACK_ASSERT(initial_rx_data.tot_len <= buf_size)
-            AIPSTACK_ASSERT(con.getRecvBuf().tot_len <= buf_size - initial_rx_data.tot_len)
-            
-            m_buf_node = IpBufNode{buf, buf_size, &m_buf_node};
-            
-            SeqType max_rx_window = MinValueU(m_buf_node.len, TcpProto::MaxRcvWnd);
-            SeqType thres = MaxValue((SeqType)1, max_rx_window / wnd_upd_div);
-            con.setWindowUpdateThreshold(thres);
-            
-            IpBufRef recv_buf = IpBufRef{&m_buf_node, (size_t)0, m_buf_node.len};
-            
-            if (initial_rx_data.tot_len > 0) {
-                recv_buf.giveBuf(initial_rx_data);
-            }
-            
-            IpBufRef old_recv_buf = con.getRecvBuf();
-            if (old_recv_buf.tot_len > 0) {
-                IpBufRef tmp_buf = recv_buf;
-                tmp_buf.giveBuf(old_recv_buf);
-            }
-            
-            con.setRecvBuf(recv_buf);
-        }
+    inline size_t getFreeLen (Connection &con) const
+    {
+        IpBufRef send_buf = getSendBuf(con);
+        return getModulo().modulusComplement(send_buf.tot_len);
+    }
+    
+    IpBufRef getWriteRange (Connection &con) const
+    {
+        IpBufRef send_buf = getSendBuf(con);
+        size_t write_offset = getModulo().add(send_buf.offset, send_buf.tot_len);
+        size_t free_len = getModulo().modulusComplement(send_buf.tot_len);
+        return IpBufRef{&m_buf_node, write_offset, free_len};
+    }
+    
+    inline void provideData (Connection &con, size_t amount)
+    {
+        AIPSTACK_ASSERT(amount <= getFreeLen(con))
         
-        size_t getUsedLen (Connection &con)
-        {
-            return m_buf_node.len - get_recv_buf(con).tot_len;
-        }
+        con.extendSendBuf(amount);
+    }
+    
+    void writeData (Connection &con, MemRef data)
+    {
+        AIPSTACK_ASSERT(data.len <= getFreeLen(con))
         
-        WrapBuffer getReadPtr (Connection &con)
-        {
-            IpBufRef rcv_buf = get_recv_buf(con);
-            size_t read_offset = add_modulo(rcv_buf.offset, rcv_buf.tot_len, m_buf_node.len);
-            return WrapBuffer(m_buf_node.len - read_offset, m_buf_node.ptr + read_offset, m_buf_node.ptr);
-        }
-        
-        void consumeData (Connection &con, size_t amount)
-        {
-            AIPSTACK_ASSERT(amount <= getUsedLen(con))
-            
-            con.extendRecvBuf(amount);
-        }
-        
-        void readData (Connection &con, MemRef data)
-        {
-            AIPSTACK_ASSERT(data.len <= getUsedLen(con))
-            
-            getReadPtr(con).copyOut(data);
-            con.extendRecvBuf(data.len);
-        }
-        
-        void updateMirrorAfterDataReceived (Connection &con, size_t mirror_size, size_t amount)
-        {
-            AIPSTACK_ASSERT(mirror_size > 0)
-            AIPSTACK_ASSERT(mirror_size < m_buf_node.len)
-            
-            if (amount > 0) {
-                // Calculate the offset in the buffer to which new data was written.
-                IpBufRef rcv_buf = con.getRecvBuf();
-                AIPSTACK_ASSERT(rcv_buf.tot_len + amount <= m_buf_node.len)
-                AIPSTACK_ASSERT(rcv_buf.offset < m_buf_node.len)
-                size_t data_offset = add_modulo(rcv_buf.offset, m_buf_node.len - amount, m_buf_node.len);
-                
-                // Copy data to the mirror region as needed.
-                if (data_offset < mirror_size) {
-                    ::memcpy(
-                        m_buf_node.ptr + m_buf_node.len + data_offset,
-                        m_buf_node.ptr + data_offset,
-                        MinValue(amount, mirror_size - data_offset));
-                }
-                if (amount > m_buf_node.len - data_offset) {
-                    ::memcpy(
-                        m_buf_node.ptr + m_buf_node.len,
-                        m_buf_node.ptr,
-                        MinValue(amount - (m_buf_node.len - data_offset), mirror_size));
-                }
-            }
-        }
-        
-    private:
-        inline IpBufRef get_recv_buf (Connection &con)
-        {
-            IpBufRef rcv_buf = con.getRecvBuf();
-            AIPSTACK_ASSERT(rcv_buf.tot_len <= m_buf_node.len)
-            AIPSTACK_ASSERT(rcv_buf.offset < m_buf_node.len) // < due to eager buffer consumption
-            return rcv_buf;
-        }
-        
-    private:
-        IpBufNode m_buf_node;
-    };
+        IpBufRef write_range = getWriteRange(con);
+        write_range.giveBytes(data.len, data.ptr);
+
+        con.extendSendBuf(data.len);
+    }
     
 private:
-    static size_t add_modulo (size_t start, size_t count, size_t modulo)
+    inline Modulo getModulo () const
     {
-        size_t x = start + count;
-        if (x >= modulo) {
-            x -= modulo;
-        }
-        return x;
+        return Modulo(m_buf_node.len);
     }
+
+    inline IpBufRef getSendBuf (Connection &con) const
+    {
+        IpBufRef send_buf = con.getSendBuf();
+
+        // Assert expectations. The second is due to eager advancement to subsequent
+        // buffer nodes guaranteed by TcpConnection.
+        AIPSTACK_ASSERT(send_buf.tot_len <= getModulo().modulus())
+        AIPSTACK_ASSERT(send_buf.offset < getModulo().modulus())
+
+        return send_buf;
+    }
+    
+private:
+    IpBufNode m_buf_node;
+};
+
+template <typename TcpProto>
+class RecvRingBuffer {
+public:
+    using Connection = typename TcpProto::Connection;
+
+    // NOTE: If using mirror region and initial_rx_data is not empty, it is
+    // you may need to call updateMirrorAfterDataReceived to make sure initial
+    // data is mirrored as applicable.
+    void setup (Connection &con, char *buf, size_t buf_size, int wnd_upd_div,
+                IpBufRef initial_rx_data = IpBufRef{})
+    {
+        AIPSTACK_ASSERT(buf != nullptr)
+        AIPSTACK_ASSERT(buf_size > 0)
+        AIPSTACK_ASSERT(wnd_upd_div >= 2)
+        AIPSTACK_ASSERT(initial_rx_data.tot_len <= buf_size)
+        AIPSTACK_ASSERT(buf_size - initial_rx_data.tot_len >= con.getRecvBuf().tot_len)
+        
+        m_buf_node = IpBufNode{buf, buf_size, &m_buf_node};
+        
+        con.setProportionalWindowUpdateThreshold(buf_size, wnd_upd_div);
+        
+        IpBufRef old_recv_buf = con.getRecvBuf();
+
+        IpBufRef recv_buf = IpBufRef{&m_buf_node, (size_t)0, buf_size};
+        
+        if (initial_rx_data.tot_len > 0) {
+            recv_buf.giveBuf(initial_rx_data);
+        }
+        
+        if (old_recv_buf.tot_len > 0) {
+            IpBufRef tmp_buf = recv_buf;
+            tmp_buf.giveBuf(old_recv_buf);
+        }
+        
+        con.setRecvBuf(recv_buf);
+    }
+    
+    inline size_t getUsedLen (Connection &con) const
+    {
+        IpBufRef recv_buf = getRecvBuf(con);
+        return getModulo().modulusComplement(recv_buf.tot_len);
+    }
+    
+    IpBufRef getReadRange (Connection &con)
+    {
+        IpBufRef recv_buf = getRecvBuf(con);
+        size_t read_offset = getModulo().add(recv_buf.offset, recv_buf.tot_len);
+        size_t used_len = getModulo().modulusComplement(recv_buf.tot_len);
+        return IpBufRef{&m_buf_node, read_offset, used_len};
+    }
+    
+    inline void consumeData (Connection &con, size_t amount)
+    {
+        AIPSTACK_ASSERT(amount <= getUsedLen(con))
+        
+        con.extendRecvBuf(amount);
+    }
+    
+    void readData (Connection &con, MemRef data)
+    {
+        AIPSTACK_ASSERT(data.len <= getUsedLen(con))
+        
+        IpBufRef read_range = getReadRange(con);
+        read_range.takeBytes(data.len, (char *)data.ptr);
+
+        con.extendRecvBuf(data.len);
+    }
+    
+    void updateMirrorAfterReceived (Connection &con, size_t mirror_size, size_t amount)
+    {
+        AIPSTACK_ASSERT(mirror_size >= 0)
+        AIPSTACK_ASSERT(mirror_size <= getModulo().modulus())
+        
+        if (AIPSTACK_UNLIKELY(amount == 0)) {
+            return;
+        }
+    
+        Modulo mod = getModulo();
+
+        IpBufRef recv_buf = con.getRecvBuf();
+        AIPSTACK_ASSERT(recv_buf.tot_len + amount <= mod.modulus())
+        AIPSTACK_ASSERT(recv_buf.offset < mod.modulus())
+
+        // Calculate the offset in the buffer to which new data was written and
+        // the number of bytes wrap-around.
+        size_t data_offset = mod.sub(recv_buf.offset, amount);
+        size_t wrap_len = mod.modulusComplement(data_offset);
+        
+        // Copy initial data to the mirror region as needed.
+        if (data_offset < mirror_size) {
+            ::memcpy(
+                m_buf_node.ptr + mod.modulus() + data_offset,
+                m_buf_node.ptr + data_offset,
+                MinValue(amount, size_t(mirror_size - data_offset)));
+        }
+
+        // Copy final data to the mirror region as needed.
+        if (amount > wrap_len) {
+            ::memcpy(
+                m_buf_node.ptr + mod.modulus(),
+                m_buf_node.ptr,
+                MinValue(size_t(amount - wrap_len), mirror_size));
+        }
+    }
+    
+private:
+    inline Modulo getModulo () const
+    {
+        return Modulo(m_buf_node.len);
+    }
+    
+    inline IpBufRef getRecvBuf (Connection &con) const
+    {
+        IpBufRef recv_buf = con.getRecvBuf();
+
+        // Assert expectations. The second is due to eager advancement to subsequent
+        // buffer nodes guaranteed by TcpConnection.
+        AIPSTACK_ASSERT(recv_buf.tot_len <= getModulo().modulus())
+        AIPSTACK_ASSERT(recv_buf.offset < getModulo().modulus())
+
+        return recv_buf;
+    }
+    
+private:
+    IpBufNode m_buf_node;
 };
 
 }
